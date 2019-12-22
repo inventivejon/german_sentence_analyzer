c3f60a049830caf8cccc0e703f7e5c0ab3c1fbc1
Jonathan Herrmann <jonathan.daniel.herrmann@gmail.com>
2019-12-22 15:06:25 +0100

    - SetUp basic elements

---
+++ SenAnAPI/SenAnAPI/Controllers/SenAnController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SenAnAPI.HostedServices;
+
+namespace SenAnAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SenAnController : ControllerBase
+    {
+        private readonly SenAnHostedService _TimedHostedService;
+
+        public SenAnController(SenAnHostedService timedHostedService)
+        {
+            _TimedHostedService = timedHostedService;
+        }
+
+        [HttpGet]
+        public int GetValue()
+        {
+            return this._TimedHostedService.GetCount();
+        }
+    }
+}
---
+++ SenAnAPI/SenAnAPI/HostedServices/SenAnHostedService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SenAnAPI.HostedServices
+{
+    public class SenAnHostedService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private readonly ILogger<SenAnHostedService> _logger;
+        private Timer _timer;
+
+        public SenAnHostedService(ILogger<SenAnHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public int GetCount()
+        {
+            return executionCount;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            executionCount++;
+
+            _logger.LogInformation(
+                "Timed Hosted Service is working. Count: {Count}", executionCount);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
